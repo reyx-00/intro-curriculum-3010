96d5f638c7155dae0017dcf6770361eaf010fb0b
rey <167632343+reyx-00@users.noreply.github.com>
2024-08-13 17:28:08 +0900

    修正

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('node:assert');
 const test = require('node:test');
+
+const fs = require('node:fs');
+fs.unlinkSync('./tasks.json');
+const todo = require('./index.js');
 
 test('addとlistのテスト', () => {
   todo.add('ノートを買う');
